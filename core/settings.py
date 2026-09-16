134b5913f37800aabd94ddc6ea283a3deba54815
fabiohanada <fabiohanada23@gmail.com>
2026-09-15 21:42:10 -0300

    Atualizar o settings.py

--- core/settings.py
+++ core/settings.py
@@ -133,7 +133,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Configuração Padrão do Gmail SMTP no Django
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
